ac4ac890590ff8dddc8773a3f180610d041a8803
EmelKrist <kristina_1516@mail.ru>
2021-11-15 18:16:58 +0800

    Adding code

---
+++ Language.java
@@ -0,0 +1,43 @@
+package compilatorv2;
+
+import java.util.Scanner;
+
+public class Language {
+    public Compiler createCompiler(){
+        //Меню выбора языка программирования
+        Scanner in = new Scanner(System.in);
+                          System.out.println("\n1.Java"
+                    + System.lineSeparator() + "2.C"
+                    + System.lineSeparator() + "3.Python");
+            System.out.print("Выберите пункт: ");
+            int languageName = in.nextInt();
+            switch (languageName) {
+                case 1:
+                    //Метод создания объекта Java компилятора
+                    return createJavaCompiler();
+                case 2:
+                    //Метод создания объекта C компилятора
+                    return createCCompiler();
+                case 3:
+                    //Метод создания объекта Python компилятора
+                    return createPythonCompiler();
+                default:
+                    System.out.println("Введен неподдерживаемый компилятором язык программирования!");
+                    return null;
+            }              
+    }
+    public JavaCompiler createJavaCompiler(){
+        //Возвращаем созданный объект класса JavaCompiler
+        return new JavaCompiler();
+    }
+
+    public CCompiler createCCompiler(){
+        //Возвращаем созданный объект класса CCompiler
+        return new CCompiler();
+    }
+
+    public PythonCompiler createPythonCompiler(){
+        //Возвращаем созданный объект класса PythonCompiler
+        return new PythonCompiler();
+    }
+}
